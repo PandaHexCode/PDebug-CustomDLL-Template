5113f3cb50ad7ff6b3843b74654fbfc40641258f
PandaHexCode <49723922+PandaHexCode@users.noreply.github.com>
2022-05-15 11:43:48 +0200

    Add files via upload

---
+++ PDebugCustomDLLTemplate/Loader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace PDebugCustomDLLTemplate{
+
+    /*PDebug CutomDLL Template for building a DLL that contains a Unity MonoBehaviour,
+    * so that PDebug can add Custom Behaviours to an GameObject.
+    */
+    /*If you want to use Functions or Types from your target Game just
+    * add the "Assembly-CSharp.dll" to your References (Dependencies - > Add Project Reference).
+    */
+
+    public class CustomBehaviour : MonoBehaviour{
+
+        private void Start(){
+            Debug.Log("CustomDLL was initialized!");
+        }
+
+        private void Update(){
+        }
+
+        private void OnGUI(){/*For easily writing custom gui*/
+        }
+
+    }
+
+}
